ff4703f03270b090d1c4e60126e4c22627706d44
Pujol Marcel <marcel.pujol@cotecna.es>
2022-11-16 21:24:45 +0100

    adding town properties enum

---
+++ src/components/enums/town-properties.enum.ts
@@ -0,0 +1,17 @@
+export enum TownProperties {
+  INE = 'ine',
+  INE6 = 'ine6',
+  NAME = 'municipi_nom',
+  SHORT_NAME = 'municipi_nom_curt',
+  COORDINATES = 'centre_municipal',
+  REGION_GROUP = 'grup_comarca',
+  REGION_CODE = 'comarca_codi',
+  REGION_NAME = 'comarca_nom',
+  LAND_GROUP = 'grup_provincia',
+  LAND_CODE = 'provincia_codi',
+  LAND_NAME = 'provincia_name',
+  LOGO = 'municipi_escut',
+  FLAG = 'municipi_bandera',
+  POPULATION = 'nombre_habitants',
+  AREA = 'extensio'
+}
