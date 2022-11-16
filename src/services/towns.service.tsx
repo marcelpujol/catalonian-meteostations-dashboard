72a2109ae866008a62125a6fc68c47748e13ae45
Marcel Pujol <mpujolnavarro@gmail.com>
2022-11-16 22:09:21 +0100

    fix

--- src/services/towns.service.tsx
+++ src/services/towns.service.tsx
@@ -2,16 +2,16 @@ import data from '../data/data.json';
 import { Town } from '../models/town.model';
 import { mapToTown } from './town.service';
 
-export const getTowns = (currentPage: number = 0, pageSize: number = 10) => {
+export const getTowns = (currentPage: number = 0, pageSize: number = 10): Town[] => {
   const result: any = data;
   const firstIndex = currentPage * pageSize;
   const secondIndex = firstIndex + pageSize;
-
   const dataSubset = result.elements.slice(firstIndex, secondIndex);
-  console.log('dataSubset', dataSubset);
+
   const towns: Town[] = [];
   for (const data of dataSubset) {
     towns.push(mapToTown(data));
   }
+  return towns;
 }
 
