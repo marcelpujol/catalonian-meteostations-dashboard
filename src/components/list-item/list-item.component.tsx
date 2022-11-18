0bf3d374efcf1c98991b799ea8f51ff644ccfc3d
Pujol Marcel <marcel.pujol@cotecna.es>
2022-11-18 08:58:43 +0100

    UI changes

--- src/components/list-item/list-item.component.tsx
+++ src/components/list-item/list-item.component.tsx
@@ -1,5 +1,7 @@
 import { Town } from "../../models/town.model"
 
+import './list-item.component.scss';
+
 interface TownListParams {
   town: Town;
 }
@@ -7,8 +9,39 @@ interface TownListParams {
 export const ListItemComponent = (params: TownListParams) => {
   return (
     <div className="town-item">
-      <p>{params.town.name}</p>
-      <img src={params.town.logo}/>
+      <img src={params.town.logo} alt="town logo"/>
+      <div className="town-info">
+        <div className="col">
+          <div className="category">
+            <p>Nom</p>
+            <p>{params.town.name}</p>
+          </div>
+          <div className="category">
+            <p>Altitud</p>
+            <p>{params.town.altitude}</p>
+          </div>
+        </div>
+        <div className="col">
+          <div className="category">
+            <p>Comarca</p>
+            <p>{params.town.region.name}</p>
+          </div>
+          <div className="category">
+            <p>Superficie (km2)</p>
+            <p>{params.town.area}</p>
+          </div>
+        </div>
+        <div className="col">
+          <div className="category">
+            <p>Provincia</p>
+            <p>{params.town.land.name}</p>
+          </div>
+          <div className="category">
+            <p>Habitants</p>
+            <p>{params.town.population}</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/list/list.component.tsx
+++ src/components/list/list.component.tsx
@@ -3,6 +3,8 @@ import { Town } from "../../models/town.model";
 import { getTowns } from '../../services/towns.service';
 import { ListItemComponent } from "../list-item/list-item.component";
 
+import './list.component.scss';
+
 export const ListComponent = () => {
   const [page, setPage] = useState(0);
   const [towns, setTowns] = useState<Town[]>([]);
@@ -12,9 +14,21 @@ export const ListComponent = () => {
     setTowns(towns);
   }, [page]);
 
+  const previousHandler = () => {
+    setPage(page - 1);
+  }
+
+  const nextHandler = () => {
+    setPage(page + 1);
+  }
+
   return (
-    <div className="town-list">
-        { towns.map(town => <ListItemComponent town={town}></ListItemComponent>) }
-    </div>
+    <>
+      <div className="town-list">
+          { towns.map(town => <ListItemComponent key={town.id} town={town}></ListItemComponent>) }
+      </div>
+      <button onClick={previousHandler}>Previous Page</button>
+      <button onClick={nextHandler}>Next Page</button>
+    </>
   );
 }
