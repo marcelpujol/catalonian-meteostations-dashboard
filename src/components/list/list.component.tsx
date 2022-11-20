9acfaf3921abae46259915767830494802c6365e
Marcel Pujol <43847889+marcelpujol@users.noreply.github.com>
2022-11-20 11:09:28 +0100

    Get towns list (#3)
    * adding town properties enum
    
    * adding local data as json
    
    * adding new models
    
    * adding town service
    
    * changes
    
    * delete enum
    
    * services refactor
    
    * fix
    
    * fix
    
    * add list item component
    
    * adding list component
    
    * fix property
    
    * adding new property
    
    * UI changes
    
    * increasing pagination
    
    * adding infinite scroll
    
    * adding id
    
    Co-authored-by: Pujol Marcel <marcel.pujol@cotecna.es>

--- src/components/list/list.component.tsx
+++ src/components/list/list.component.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useRef, useState } from "react"
 import { Town } from "../../models/town.model";
 import { getTowns } from '../../services/towns.service';
 import { ListItemComponent } from "../list-item/list-item.component";
@@ -9,26 +9,42 @@ export const ListComponent = () => {
   const [page, setPage] = useState(0);
   const [towns, setTowns] = useState<Town[]>([]);
 
+  const observer: any = useRef();
+  const lastTownElementRef = useCallback((node: any) => {
+    if (observer.current) observer.current.disconnect();
+    observer.current = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting) {
+        setPage(page => page + 1);
+      }
+    });
+    if (node) observer.current.observe(node);
+  },[])
+
   useEffect(() => {
-    const towns = getTowns(page);
-    setTowns(towns);
+    let ignoreCall = false;
+    setTimeout(() => {
+      const towns = getTowns(page);
+      if (!ignoreCall) {
+        setTowns(prev => [...prev, ...towns]);
+      }
+    }, 0);
+    return () => { ignoreCall = true };
   }, [page]);
 
-  const previousHandler = () => {
-    setPage(page - 1);
-  }
-
-  const nextHandler = () => {
-    setPage(page + 1);
+  const getTownItem = (town: Town, index: number) => {
+    const isLastElement = towns.length === index + 1;
+    return isLastElement
+      ? <div ref={lastTownElementRef} key={town.id}></div>
+      : <ListItemComponent key={town.id} town={town}></ListItemComponent>
   }
 
   return (
     <>
       <div className="town-list">
-          { towns.map(town => <ListItemComponent key={town.id} town={town}></ListItemComponent>) }
+          { 
+            towns.map((town, index) => getTownItem(town, index))
+          }
       </div>
-      <button onClick={previousHandler}>Previous Page</button>
-      <button onClick={nextHandler}>Next Page</button>
     </>
   );
 }
--- src/services/towns.service.tsx
+++ src/services/towns.service.tsx
@@ -2,7 +2,7 @@ import data from '../data/data.json';
 import { Town } from '../models/town.model';
 import { mapToTown } from './town.service';
 
-export const getTowns = (currentPage: number = 0, pageSize: number = 10): Town[] => {
+export const getTowns = (currentPage: number = 0, pageSize: number = 20): Town[] => {
   const result: any = data;
   const firstIndex = currentPage * pageSize;
   const secondIndex = firstIndex + pageSize;
