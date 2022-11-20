e49e0503bd71063889e5f8b7ca2d67353bd0d63c
Pujol Marcel <marcel.pujol@cotecna.es>
2022-11-20 11:15:30 +0100

    adding search

---
+++ src/components/search/search.component.tsx
@@ -0,0 +1,10 @@
+export const SearchComponent = () => {
+  return (
+    <>
+      <div>
+        <label>Search something here...</label>
+        <input type="text"></input>
+      </div>
+    </>
+  );
+}
