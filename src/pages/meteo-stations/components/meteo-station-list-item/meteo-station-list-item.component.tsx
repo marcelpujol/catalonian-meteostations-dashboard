caf625f78c106d8d2b79561339d592f5901d38c7
Marcel Pujol <mpujolnavarro@gmail.com>
2022-12-04 20:01:17 +0100

    adding meteo station list item

---
+++ src/pages/meteo-stations/components/meteo-station-list-item/meteo-station-list-item.component.tsx
@@ -0,0 +1,22 @@
+import { MeteoStation } from '../../../../models/meteo-station.model';
+
+import './meteo-station-list-item.component.scss';
+
+type Props = {
+    meteoStation: MeteoStation
+}
+
+export const MeteoStationListItemComponent = ({ meteoStation }: Props) => {
+    return (
+        <div className="meteo-station__container">
+            <div className="meteo-station__row first-line">
+                <span>{meteoStation.name}</span>
+                <span>{meteoStation.code}</span>
+            </div>
+            <div className="meteo-station__row">
+                <span>{meteoStation.town.name}</span>
+                <span>{meteoStation.region.name}</span>
+            </div>
+        </div>
+    )
+}
