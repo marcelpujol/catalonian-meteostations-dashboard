beb293ecf43b349845989ab7846da3db09e31870
Marcel Pujol <mpujolnavarro@gmail.com>
2022-12-04 20:01:28 +0100

    adding meteostation service

--- src/services/meteo-stations.service.ts
+++ src/services/meteo-stations.service.ts
@@ -3,15 +3,19 @@ import { Catalogue } from './../models/catalogue.model';
 import { MeteoStationProps } from '../enums/meteo-station-props.enum';
 import { MeteoStation } from '../models/meteo-station.model';
 
+const DISABLED_STATUS_CODE: string = "1";
+
 export const getMeteoStations = (): Promise<MeteoStation[]> => {
     return new Promise<MeteoStation[]>((resolve, reject) => {
         try {
             const meteoStations: MeteoStation[] = [];
-            const results: any = data;
+            const results: any = data.elements;
             for (const result of results) {
                 const meteoStation = mapToMeteoStation(result);
-                meteoStations.push(meteoStation);
+                if (meteoStation.state.code !== DISABLED_STATUS_CODE)
+                    meteoStations.push(meteoStation);
             }
+            meteoStations.sort(sortAlgorithm);
             resolve(meteoStations)
         }
         catch(err) {
@@ -36,4 +40,9 @@ const mapToCatalogues = (data: any, codeProperty: string, nameProperty: string) 
         code: data[codeProperty],
         name: data[nameProperty]
     } as Catalogue;
+}
+
+const sortAlgorithm = (a: MeteoStation, b: MeteoStation) => {
+    if (a.town.name < b.town.name) return -1;
+    else return 1;
 }
