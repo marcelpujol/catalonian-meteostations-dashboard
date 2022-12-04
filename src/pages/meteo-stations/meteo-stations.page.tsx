e01218e341542a1cc72506b070d2aee16de1a5d0
Marcel Pujol <mpujolnavarro@gmail.com>
2022-12-04 20:00:22 +0100

    adding pages

---
+++ src/pages/meteo-stations/meteo-stations.page.tsx
@@ -0,0 +1,9 @@
+import { MeteoStationListComponent } from "./components/meteo-stations-list/meteo-station-list.component"
+
+export const MeteoStationsPage = () => {
+    return (
+        <>
+            <MeteoStationListComponent/>
+        </>
+    )
+}
