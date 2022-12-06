5b44de31506ef1c19fb424b0f06d9bb04a6a80d4
Marcel Pujol <mpujolnavarro@gmail.com>
2022-12-06 12:09:22 +0100

    fixes and some improvements

--- src/pages/meteo-stations/components/meteo-stations-list/meteo-station-list.component.tsx
+++ src/pages/meteo-stations/components/meteo-stations-list/meteo-station-list.component.tsx
@@ -1,19 +1,35 @@
 import { useEffect, useState } from "react"
+import { MeteoStationVariable } from "../../../../models/meteo-station-variable.model";
 import { MeteoStation } from "../../../../models/meteo-station.model";
+import { getMeteoMetadata } from "../../../../services/meteo-metadata.service";
 import { getMeteoStations } from "../../../../services/meteo-stations.service";
 import { MeteoStationListItemComponent } from "../meteo-station-list-item/meteo-station-list-item.component";
 
 import './meteo-station-list.component.scss';
 
 export const MeteoStationListComponent = () => {
-    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
     const [meteoStations, setMeteoStations] = useState<MeteoStation[]>([]);
+    const [meteoMetadata, setMeteoMetaData] = useState<MeteoStationVariable[]>([]);
 
     useEffect(() => {
-        getMeteoStations()
-            .then((results) => setMeteoStations(results))
-            .catch((err) => console.error('err', err))
-            .finally(() => setIsLoading(false))
+        let initialLoading: boolean = true;
+        setTimeout(() => {
+            if (initialLoading) {
+                Promise.all([
+                    getMeteoStations(),
+                    getMeteoMetadata()
+                ]).then(([meteoStations, meteoMetadata]) => {
+                    setMeteoStations(meteoStations);
+                    setMeteoMetaData(meteoMetadata);
+                }).catch((err) => {
+                    console.error('err', err);
+                }).finally(() => {
+                    setIsLoading(false);
+                });
+            }
+        });
+        return () => { initialLoading = false; }
     }, []);
 
     return (
