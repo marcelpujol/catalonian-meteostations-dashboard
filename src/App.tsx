8823fabeb66e564918c95742953d910324290dfe
Marcel Pujol <mpujolnavarro@gmail.com>
2022-12-13 18:10:18 +0100

    adding router and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { ListComponent } from './components/list/list.component';
 import { MeteoStationsPage } from './pages/meteo-stations/meteo-stations.page';
 
 function App() {
   return (
-    <div>
-      {/* <ListComponent></ListComponent> */}
-      <MeteoStationsPage></MeteoStationsPage>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ListComponent/>}/>
+        <Route path="towns" element={<ListComponent/>}/>
+        <Route path="meteostations" element={<MeteoStationsPage/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
