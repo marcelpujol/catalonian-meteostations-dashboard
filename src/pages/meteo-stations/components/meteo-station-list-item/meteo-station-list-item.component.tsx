bf5f6f7b36bee2181cf8ca5531d2455a53cc7de1
Marcel Pujol <mpujolnavarro@gmail.com>
2022-12-14 19:14:23 +0100

    adding page redirection

--- src/pages/meteo-stations/components/meteo-station-list-item/meteo-station-list-item.component.tsx
+++ src/pages/meteo-stations/components/meteo-station-list-item/meteo-station-list-item.component.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import { MeteoStation } from '../../../../models/meteo/meteo-station.model';
 import './meteo-station-list-item.component.scss';
 
@@ -6,8 +7,15 @@ type Props = {
 }
 
 export const MeteoStationListItemComponent = ({ meteoStation }: Props) => {
+    const navigate = useNavigate();
+
+    const handleClick = (meteoStationCode: string) => {
+        console.log('click', meteoStationCode);
+        navigate(`/meteodata/${meteoStationCode}`);
+    }
+
     return (
-        <div className="meteo-station__container" key={meteoStation.code}>
+        <div className="meteo-station__container" key={meteoStation.code} onClick={() => { handleClick(meteoStation.code) }}>
             <div className="meteo-station__row first-line">
                 <span>{meteoStation.name}</span>
                 <span>{meteoStation.code}</span>
