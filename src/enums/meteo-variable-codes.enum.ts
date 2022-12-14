1b375340808f8813b07eaaf7cb1484dc71812212
Marcel Pujol <mpujolnavarro@gmail.com>
2022-12-14 19:12:06 +0100

    adding enums for variable codes

---
+++ src/enums/meteo-variable-codes.enum.ts
@@ -0,0 +1,31 @@
+export const enum MeteoVariableCodes {
+    MAX_ATMOSPHERIC_PRESSURE = "1",
+    MIN_ATMOSPHERIC_PRESSURE = "2",
+    MAX_RELATIVE_HUMIDITY = "3",
+    WIND_VELOCITY = "30",
+    WIND_DIRECTION = "31",
+    TEMPERATURE = "32",
+    RELATIVE_HUMIDITY = "33",
+    ATHMOSPHERIC_PRESSURE = "34",
+    RAIN = "35",
+    GLOBAL_SOLAR_IRRADIANCE = "36",
+    SNOW_LEVEL = "38",
+    MAX_TEMPERATURE = "40",
+    MIN_TEMPERATURE = "42"
+}
+
+export const enum MeteoVariableLabels {
+    MAX_ATMOSPHERIC_PRESSURE = "Max atmospheric pressure",
+    MIN_ATMOSPHERIC_PRESSURE = "Min atmospheric pressure",
+    MAX_RELATIVE_HUMIDITY = "Max relative humidity",
+    WIND_VELOCITY = "Wind velocity",
+    WIND_DIRECTION = "Wind direction",
+    TEMPERATURE = "Temperature",
+    RELATIVE_HUMIDITY = "Relative humidity",
+    ATHMOSPHERIC_PRESSURE = "AtmosphericPressure",
+    RAIN = "Rain",
+    GLOBAL_SOLAR_IRRADIANCE = "Global solar irradiance",
+    SNOW_LEVEL = "Snow level",
+    MAX_TEMPERATURE = "Max temperature",
+    MIN_TEMPERATURE = "Min temperature"
+}
