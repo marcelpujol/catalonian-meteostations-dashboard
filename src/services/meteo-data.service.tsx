4ca6994b4ac29f3271f4a4f0871b66ca8da75b6e
Marcel Pujol <mpujolnavarro@gmail.com>
2022-12-16 15:33:22 +0100

    get and parse meteostation data by displayed variables

--- src/services/meteo-data.service.tsx
+++ src/services/meteo-data.service.tsx
@@ -1,18 +1,20 @@
 import { MeteoStationDataProps } from "../enums/meteo-station-data-props.enum";
 import { MeteoStationData } from "../models/meteo/meteo-station-data.model";
-import { MeteoVariableCodes } from "../enums/meteo-variable-codes.enum";
+import { getStoredMetadataToDisplay } from "./meteo-metadata.service";
+import { MeteoStationVariable } from "../models/meteo/meteo-station-variable.model";
 
 const URL = 'https://analisi.transparenciacatalunya.cat/resource/nzvn-apee.json';
 export const getMeteoData = (stationCode: string): Promise<MeteoStationData[]> => {
-    return new Promise<MeteoStationData[]>((resolve, reject) => {
-        fetch(getUrl(stationCode))
+    return new Promise<MeteoStationData[]>(async (resolve, reject) => {
+        const maxDate = await getMaxReadingDate(stationCode) ?? getDefaultDate();
+        const metaVariablesToDisplay = await getStoredMetadataToDisplay();
+
+        const url = await getUrl(stationCode, metaVariablesToDisplay, maxDate);
+        fetch(url)
             .then(response => response.json())
             .then(results => {
-                const metaStationData: MeteoStationData[] = [];
-                results.forEach((data: any) => {
-                    metaStationData.push(mapToMeteoStationData(data));
-                });
-                resolve(metaStationData);
+                const parsedData = parseMeteoStationDataResults(results, metaVariablesToDisplay);
+                resolve(parsedData);
             })
             .catch((err) => {
                 console.error('err', err);
@@ -21,31 +23,63 @@ export const getMeteoData = (stationCode: string): Promise<MeteoStationData[]> =
     });
 }
 
-const getUrl = (stationCode: string): string => {
-    const url = `${URL}?$query=${getURLQuery(stationCode)}`;
+const getMaxReadingDate = (stationCode: string): Promise<string | null> => {
+    return new Promise<string | null>((resolve, reject) => {
+        const url = `${URL}?$select=MAX(data_lectura) as max_date&$where=codi_estacio='${stationCode}'`;
+        fetch(url)
+            .then(response => response.json())
+            .then(result => {
+                result?.length 
+                    ? resolve(result[0].max_date) 
+                    : resolve(null);
+            })
+            .catch((err) => {
+                console.error('err', err);
+                reject(err);
+            });
+    }); 
+}
+
+const getDefaultDate = (): string => {
+    let date = new Date();
+    date.setUTCHours(0,0,0,0);
+    return date.toISOString().replace('Z', '');
+}
+
+const getUrl = async (stationCode: string, metadataToDisplay: any[], maxDate: string): Promise<string> => {
+    const metadataCodes = metadataToDisplay.map(metadata => `'${metadata.code}'`).join(',');
+    const url = `${URL}?$query=${getURLQuery(stationCode, metadataCodes, maxDate)}`;
     console.log('url', url);
     return url;
 }
 
-const getURLQuery = (stationCode: string): string => {
+const getURLQuery = (stationCode: string, metadataCodes: string, maxDate: string): string => {
     return `SELECT * 
-            WHERE codi_variable = '${MeteoVariableCodes.TEMPERATURE}' OR
-                codi_variable = '${MeteoVariableCodes.MAX_TEMPERATURE}' OR
-                codi_variable = '${MeteoVariableCodes.MIN_TEMPERATURE}' OR
-                codi_variable = '${MeteoVariableCodes.RAIN}' AND
-                codi_estacio = '${stationCode}'
-            ORDER BY data_lectura DESC LIMIT 1`;
+            WHERE codi_variable IN (${metadataCodes}) 
+            AND codi_estacio = '${stationCode}' 
+            AND data_lectura = '${maxDate}'`;
 }
 
-const mapToMeteoStationData = (data: any) => {
+const parseMeteoStationDataResults = (results: any[], meteoVariablesToDisplay: MeteoStationVariable[]) => {
+    const meteoStationData: MeteoStationData[] = [];
+    results.forEach((data: any) => {
+        const code = data[MeteoStationDataProps.VARIABLE_CODE];
+        const variableToDisplay = meteoVariablesToDisplay.find(metadata => metadata.code === code);
+        meteoStationData.push(mapToMeteoStationData(data, variableToDisplay?.unit!, variableToDisplay?.name!));
+    });
+    return meteoStationData;
+}
+
+const mapToMeteoStationData = (data: any, unit: string, label: string) => {
     return {
         id: data[MeteoStationDataProps.ID],
-        station_code: data[MeteoStationDataProps.STATE_CODE],
+        station_code: data[MeteoStationDataProps.STATION_CODE],
         variable_code: data[MeteoStationDataProps.VARIABLE_CODE],
         lecture_data: data[MeteoStationDataProps.LECTURE_DATA],
         extreme_data: data[MeteoStationDataProps.EXTREME_DATA],
         value: data[MeteoStationDataProps.VALUE],
-        state_code: data[MeteoStationDataProps.STATE_CODE],
-        base_code: data[MeteoStationDataProps.BASE_CODE]
+        base_code: data[MeteoStationDataProps.BASE_CODE],
+        unit,
+        label
     } as MeteoStationData;
 }
