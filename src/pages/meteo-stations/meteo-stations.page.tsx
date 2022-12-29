93ca76b2436029af864fadc8cab84dabe02a6548
Marcel Pujol <mpujolnavarro@gmail.com>
2022-12-29 09:42:29 +0100

    integrate search component

--- src/pages/meteo-stations/meteo-stations.page.tsx
+++ src/pages/meteo-stations/meteo-stations.page.tsx
@@ -1,8 +1,15 @@
+import { SearchComponent } from "../../components/search/search.component"
 import { MeteoStationListComponent } from "./components/meteo-stations-list/meteo-station-list.component"
 
 export const MeteoStationsPage = () => {
+
+    function handleChangeSearch(value: string) {
+        console.log('handleChangeSearch', value);
+    }
+
     return (
         <>
+            <SearchComponent onChangeSearch={handleChangeSearch}/>
             <MeteoStationListComponent/>
         </>
     )
