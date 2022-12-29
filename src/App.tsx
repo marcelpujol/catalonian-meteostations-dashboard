ac311535ad19163ef40f64d13d7652000d29a776
Marcel Pujol <mpujolnavarro@gmail.com>
2022-12-29 09:42:41 +0100

    integrate toolbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { TownsPage } from './pages/towns/towns.page';
 import { MeteoStationsPage } from './pages/meteo-stations/meteo-stations.page';
 import { MeteoDataPage } from './pages/meteo-data/meteo-data.page';
@@ -7,6 +8,7 @@ import './App.css';
 function App() {
   return (
     <BrowserRouter>
+      <ToolbarComponent></ToolbarComponent>
       <Routes>
         <Route path="/" element={<TownsPage/>}/>
         <Route path="towns" element={<TownsPage/>}/>
