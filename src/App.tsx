3629305b06cb19fb73d8eaf56f262ed3a8bb2d31
Marcel Pujol <mpujolnavarro@gmail.com>
2022-12-30 16:37:25 +0100

    integrate shell component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { ShellComponent } from './components/shell/shell.component';
 import { TownsPage } from './pages/towns/towns.page';
 import { MeteoStationsPage } from './pages/meteo-stations/meteo-stations.page';
 import { MeteoDataPage } from './pages/meteo-data/meteo-data.page';
@@ -8,13 +8,14 @@ import './App.css';
 function App() {
   return (
     <BrowserRouter>
-      <ToolbarComponent></ToolbarComponent>
-      <Routes>
-        <Route path="/" element={<TownsPage/>}/>
-        <Route path="towns" element={<TownsPage/>}/>
-        <Route path="meteostations" element={<MeteoStationsPage/>}/>
-        <Route path="meteodata/:id" element={<MeteoDataPage/>}/>
-      </Routes>
+      <ShellComponent>
+        <Routes>
+          <Route path="/" element={<TownsPage/>}/>
+          <Route path="towns" element={<TownsPage/>}/>
+          <Route path="meteostations" element={<MeteoStationsPage/>}/>
+          <Route path="meteodata/:id" element={<MeteoDataPage/>}/>
+        </Routes>
+      </ShellComponent>
     </BrowserRouter>
   );
 }
