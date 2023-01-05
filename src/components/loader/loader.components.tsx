c0bd985ed7e943e3fb8c62c432a97ceec6a67dd7
Marcel Pujol <mpujolnavarro@gmail.com>
2023-01-05 17:58:31 +0100

    adding loader component

---
+++ src/components/loader/loader.components.tsx
@@ -0,0 +1,10 @@
+import './loader.component.scss';
+
+export const LoaderComponent = () => {
+    return (
+        <div className="lds-ripple">
+            <div></div>
+            <div></div>
+        </div>
+    );
+}
