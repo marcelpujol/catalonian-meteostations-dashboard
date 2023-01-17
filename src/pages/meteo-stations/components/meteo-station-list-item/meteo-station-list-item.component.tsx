4062baf24bad684dec3cfbec364489edbc715109
Marcel Pujol <mpujolnavarro@gmail.com>
2023-01-17 18:58:16 +0100

    use custom hook to update the toolbar state

--- src/pages/meteo-stations/components/meteo-station-list-item/meteo-station-list-item.component.tsx
+++ src/pages/meteo-stations/components/meteo-station-list-item/meteo-station-list-item.component.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom';
+import { useToolbar } from '../../../../hooks/useUpdateToolbar.hook';
 import { MeteoStation } from '../../../../models/meteo/meteo-station.model';
 import './meteo-station-list-item.component.scss';
 
@@ -8,10 +9,16 @@ type Props = {
 
 export const MeteoStationListItemComponent = ({ meteoStation }: Props) => {
     const navigate = useNavigate();
+    const { updateToolbar } = useToolbar();
 
-    const handleClick = (meteoStationCode: string) => {
+    function handleClick(meteoStationCode: string) {
         console.log('click', meteoStationCode);
         navigate(`/meteodata/${meteoStationCode}`);
+        updateToolbarTitle();
+    }
+
+    function updateToolbarTitle() {
+        updateToolbar(meteoStation.name, true);
     }
 
     return (
