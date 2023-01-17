211302c8e648d069b87a1bd84064eb0d45ceb000
Marcel Pujol <mpujolnavarro@gmail.com>
2023-01-17 18:56:13 +0100

    adding new properties to the toolbar state

--- src/redux/toolbarSlice.tsx
+++ src/redux/toolbarSlice.tsx
@@ -3,11 +3,13 @@ import { createSlice } from "@reduxjs/toolkit";
 export const toolbarSlice = createSlice({
     name: 'toolbar',
     initialState: {
-        title: 'Towns'
+        title: 'Towns',
+        backArrow: false
     },
     reducers: {
         update: (state, action) => {
             state.title = action.payload.title;
+            state.backArrow = action.payload.backArrow;
         }
     }
 });
