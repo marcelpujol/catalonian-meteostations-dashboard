fbf2e4aa223d9d3ee9b4afe11c02f07e7ee3aeea
Marcel Pujol <mpujolnavarro@gmail.com>
2023-01-19 23:39:09 +0100

    integrate toggle component

--- src/components/shell/components/toolbar/toolbar.component.tsx
+++ src/components/shell/components/toolbar/toolbar.component.tsx
@@ -1,10 +1,13 @@
 import { openSideNav } from '../../utils/shell.utils';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router';
+import { useTheme } from '../../../../hooks/useTheme.hook';
+import { ToggleComponent } from '../../../toggle/toggle.component';
 import './toolbar.component.scss';
 
 export const ToolbarComponent = () => {
     const navigate = useNavigate();
+    const { updateTheme } = useTheme();
     const title = useSelector<any, any>(state => state.toolbar.title);
     const backArrow = useSelector<any, any>(state => state.toolbar.backArrow);
 
@@ -16,10 +19,24 @@ export const ToolbarComponent = () => {
         navigate(-1);
     }
 
+    function onToggleChanged(event: any) {
+        updateTheme(event);
+    }
+
     function getToolbarIcon(): any {
         return backArrow 
             ? <span className="material-symbols-outlined" onClick={handleBackClick}>arrow_back</span>
             : <span className="open-button material-symbols-outlined" onClick={handleMenuClick}>menu</span>;
+    }
+
+    function getThemeMode(): any {
+        return (
+            <div className="theme-container">
+                <span className="material-symbols-outlined icon">light_mode</span>
+                <ToggleComponent toggleChanged={onToggleChanged}></ToggleComponent>
+                <span className="material-symbols-outlined icon">dark_mode</span>
+            </div>
+        );
     }
 
     return (
@@ -27,6 +44,8 @@ export const ToolbarComponent = () => {
             <div className="content">
                 { getToolbarIcon() }
                 <p>{title}</p>
+                <span className="separator"/>
+                { getThemeMode() }
             </div>
         </div>
     )
