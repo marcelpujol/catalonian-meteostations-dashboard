89680087f23331f94a4e6d8dcc936059ca7e9300
Marcel Pujol <mpujolnavarro@gmail.com>
2023-01-25 22:18:25 +0100

    adding meteo variables constants

--- src/constants/meteo-variables.constants.ts
+++ src/constants/meteo-variables.constants.ts
@@ -1,6 +1,7 @@
 import { MeteoVariableCodes, MeteoVariableFamily, MeteoVariableLabels } from "../enums/meteo-variable-codes.enum";
+import { MeteoVariable } from "../models/meteo/meteo-variable.model";
 
-export const METEO_VARIABLES: MeteoVariables[] = [
+export const METEO_VARIABLES: MeteoVariable[] = [
     { code: MeteoVariableCodes.MAX_ATMOSPHERIC_PRESSURE, label: MeteoVariableLabels.MAX_ATMOSPHERIC_PRESSURE, family: MeteoVariableFamily.ATMOSPHERIC_PRESSURE },
     { code: MeteoVariableCodes.MIN_ATMOSPHERIC_PRESSURE, label: MeteoVariableLabels.MIN_ATMOSPHERIC_PRESSURE, family: MeteoVariableFamily.ATMOSPHERIC_PRESSURE },
     { code: MeteoVariableCodes.ATHMOSPHERIC_PRESSURE, label: MeteoVariableLabels.ATHMOSPHERIC_PRESSURE, family: MeteoVariableFamily.ATMOSPHERIC_PRESSURE },
@@ -15,9 +16,3 @@ export const METEO_VARIABLES: MeteoVariables[] = [
     { code: MeteoVariableCodes.GLOBAL_SOLAR_IRRADIANCE, label: MeteoVariableLabels.GLOBAL_SOLAR_IRRADIANCE, family: MeteoVariableFamily.SOLAR },
     { code: MeteoVariableCodes.SNOW_LEVEL, label: MeteoVariableLabels.SNOW_LEVEL, family: MeteoVariableFamily.SNOW }
 ];
-
-interface MeteoVariables {
-    code: string;
-    label: string;
-    family: string;
-}
