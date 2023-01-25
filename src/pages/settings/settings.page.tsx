fad1de8c78e6d724874e88f19760e57b70d6fcf6
Marcel Pujol <mpujolnavarro@gmail.com>
2023-01-25 22:17:57 +0100

    adding settings page

--- src/pages/settings/settings.page.tsx
+++ src/pages/settings/settings.page.tsx
@@ -1,7 +1,36 @@
+import { useState } from "react"
+import { ToggleComponent } from "../../components/toggle/toggle.component";
+import { METEO_VARIABLES } from "../../constants/meteo-variables.constants"
+import { MeteoVariable } from "../../models/meteo/meteo-variable.model";
+
+import './settings.page.scss';
+
 export const SettingsPage = () => {
+    const [meteoVariables, setMeteoVariables] = useState<MeteoVariable[]>(METEO_VARIABLES);
+
+    function onToggleChanged(value: boolean) {
+        console.log('checked', value);
+    }
+
+    function getMeteoVariablesContent(meteoVariable: MeteoVariable): any {
+        return (
+            <div key={`meteovariable-${meteoVariable.code}`} className="meteovariable-container">
+                <div className="first-column">
+                    <p>{ meteoVariable.label }</p>
+                </div>
+                <div className="second-column">
+                    <ToggleComponent toggleChanged={onToggleChanged}></ToggleComponent>
+                </div>
+            </div>
+        );
+    }
+
     return (
-        <div>
-            All the options that we have:
+        <div className="meteovariables-container">
+            <p className="text-info">Select the meteo variables that you want to consult on the town detail page:</p>
+            <div className="meteovariables-list">
+                { meteoVariables.map(meteoVariable => getMeteoVariablesContent(meteoVariable)) }
+            </div>
         </div>
     )
 }
