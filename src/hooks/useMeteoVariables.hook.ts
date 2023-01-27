7f1fe61b0963ea5910a4a42449277ef7a8d84bbe
Marcel Pujol <mpujolnavarro@gmail.com>
2023-01-27 21:52:02 +0100

    adding custom hook

--- src/hooks/useMeteoVariables.hook.ts
+++ src/hooks/useMeteoVariables.hook.ts
@@ -1,7 +1,7 @@
 import { MeteoVariable } from './../models/meteo/meteo-variable.model';
 import { useMemo } from "react";
 import { useDispatch } from "react-redux";
-import { update, get } from "../redux/meteoVariablesSlice";
+import { update } from "../redux/meteoVariablesSlice";
 
 export const useSelectedMeteoVariables = () => {
     const dispatch = useDispatch();
@@ -9,10 +9,6 @@ export const useSelectedMeteoVariables = () => {
     const actions = useMemo(() => ({
         update(updatedMeteoVariable: MeteoVariable) {
             const action = update({ updated: updatedMeteoVariable });
-            dispatch(action);
-        },
-        get() {
-            const action = get();
             dispatch(action);
         }
     }), [dispatch])
