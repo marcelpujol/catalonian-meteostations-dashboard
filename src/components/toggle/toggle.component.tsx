c9b1bf1afad3e7e52be7b95fa2e584ad2aee2f48
Marcel Pujol <mpujolnavarro@gmail.com>
2023-01-27 22:18:09 +0100

    adding state

--- src/components/toggle/toggle.component.tsx
+++ src/components/toggle/toggle.component.tsx
@@ -1,8 +1,10 @@
+import { useEffect, useState } from 'react';
 import { newGuid } from '../../utils/guid.utils';
 import './toggle.component.scss';
 import { ToggleProps } from './toggle.props';
 
 export const ToggleComponent = ({toggleChanged: onToggleChanged, id, defaultValue}: ToggleProps) => {
+    const [state, setState] = useState({ value: defaultValue || false });
 
     function getId() {
         return id ? id : newGuid();
@@ -10,12 +12,13 @@ export const ToggleComponent = ({toggleChanged: onToggleChanged, id, defaultValu
 
     function handleChange(e: React.ChangeEvent) {
         const element: any = e.target;
+        setState({ value: element.checked });
         onToggleChanged(element.checked, element.id);
     }
 
     return (
         <label className="switch">
-            <input type="checkbox" id={getId()} checked={defaultValue} onChange={e => handleChange(e)}/>
+            <input type="checkbox" id={getId()} checked={state.value} onChange={e => handleChange(e)}/>
             <span className="slider round"/>
         </label>
     )
