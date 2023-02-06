077e3893f322f5ab8fed6afc6f4c2ae5fdff9c08
Marcel Pujol <mpujolnavarro@gmail.com>
2023-02-06 22:49:18 +0100

    set default selected meteo variables

--- src/redux/meteoVariablesSlice.tsx
+++ src/redux/meteoVariablesSlice.tsx
@@ -1,10 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { MeteoVariableCodes, MeteoVariableFamily, MeteoVariableLabels } from "../enums/meteo-variable-codes.enum";
 import { MeteoVariable } from "../models/meteo/meteo-variable.model";
 
 export const meteoVariablesSlice = createSlice({
     name: 'meteoVariables',
     initialState: {
-        selected: []
+        selected: _getDefaultSelectedMeteoVariables()
     },
     reducers: {
         update: (state, action) => {
@@ -21,6 +22,16 @@ function _handleUpdatedMeteoVariable(state: any, action: any): void {
     (index >= 0 && !updatedMeteoVariable.selected) 
         ? meteoVariables.splice(index, 1)
         : meteoVariables.push(updatedMeteoVariable); 
+}
+
+function _getDefaultSelectedMeteoVariables(): MeteoVariable[] {
+    return [
+        { code: MeteoVariableCodes.RELATIVE_HUMIDITY, label: MeteoVariableLabels.RELATIVE_HUMIDITY, family: MeteoVariableFamily.HUMIDITY, selected: true },
+        { code: MeteoVariableCodes.TEMPERATURE, label: MeteoVariableLabels.TEMPERATURE, family: MeteoVariableFamily.TEMPERATURE, selected: true },
+        { code: MeteoVariableCodes.MAX_TEMPERATURE, label: MeteoVariableLabels.MAX_TEMPERATURE, family: MeteoVariableFamily.TEMPERATURE, selected: true },
+        { code: MeteoVariableCodes.MIN_TEMPERATURE, label: MeteoVariableLabels.MIN_TEMPERATURE, family: MeteoVariableFamily.TEMPERATURE, selected: true },
+        { code: MeteoVariableCodes.RAIN, label: MeteoVariableLabels.RAIN, family: MeteoVariableFamily.RAIN, selected: true },
+    ];
 }
 
 export const { update } = meteoVariablesSlice.actions;
