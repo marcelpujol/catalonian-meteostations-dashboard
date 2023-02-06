4f2bca1c0602e731cf7134d0baf8945205e127c1
Marcel Pujol <43847889+marcelpujol@users.noreply.github.com>
2023-02-06 23:23:56 +0100

    Fixes and some improvements (#24)
    * fix toggle load value
    
    * fix settings page
    
    * set default selected meteo variables
    
    * adding error primary color
    
    * fix
    
    * adding styles
    
    * adding error handling
    
    * adding about page + styles
    
    * integrate about page

---
+++ src/pages/about/about.page.tsx
@@ -0,0 +1,12 @@
+import './about.scss';
+
+export const AboutPage = () => {
+    return (
+        <div>
+            <div className="about-item">
+                <span>2023/02/06 - 0.0.1</span>
+                <small>First version</small>
+            </div>
+        </div>
+    );
+}
