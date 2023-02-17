c3d52686c84ae1910d7aa5c2273b2948067e9fef
Marcel Pujol <mpujolnavarro@gmail.com>
2023-02-17 15:02:11 +0100

    rename constants

--- src/constants/meteo-chart-options.constants.ts
+++ src/constants/meteo-chart-options.constants.ts
@@ -2,7 +2,7 @@ import { SelectOption } from "../components/select/select.props";
 import { MeteoVariableCodes } from "../enums/meteo-variable-codes.enum";
 
 export const METEO_CHART_OPTIONS: SelectOption[] = [
-    { name: 'Temperatura', value: MeteoVariableCodes.TEMPERATURE },
-    { name: 'Humitat relativa', value: MeteoVariableCodes.RELATIVE_HUMIDITY },
-    { name: 'Precipitació', value: MeteoVariableCodes.RAIN }
+    { name: 'Temperature', value: MeteoVariableCodes.TEMPERATURE },
+    { name: 'Relative Humidity', value: MeteoVariableCodes.RELATIVE_HUMIDITY },
+    { name: 'Rain', value: MeteoVariableCodes.RAIN }
 ] 
